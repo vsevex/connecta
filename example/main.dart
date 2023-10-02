7a04751df1eca114902b40cb0f8712bacb7c1a96
vsevex <vsevex@gmail.com>
2023-10-02 21:09:35 +0400

    example on connection upgrade

--- example/main.dart
+++ example/main.dart
@@ -1,6 +1,18 @@
+import 'dart:developer';
+
 import 'package:connecta/connecta.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+Future<void> main() async {
+  final connecta = Connecta(
+    ConnectaToolkit(
+      hostname: 'localhost',
+      certificatePath: 'public/cert.pem',
+      keyPath: 'public/key.pem',
+    ),
+  );
+
+  await connecta.connect(onData: (value) => log(value.toString()));
+  await connecta.upgradeConnection();
+
+  connecta.send('hert');
 }
