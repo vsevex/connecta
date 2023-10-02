bbb0c8026d7110c1123f700bf1387fc71f0bfbed
vsevex <vsevex@gmail.com>
2023-10-02 21:08:35 +0400

    connecta base `socket` class

---
+++ lib/src/socket.dart
@@ -0,0 +1,33 @@
+part of 'connecta.dart';
+
+abstract class ConnectaSocket {
+  late io.Socket? ioSocket;
+  late final String _hostname;
+  late final int _port;
+  late StreamSubscription subscription;
+
+  void initialize({required String hostname, required int port}) {
+    _hostname = hostname;
+    _port = port;
+  }
+
+  Future<io.Socket> createSocket({
+    void Function(List<int>)? onData,
+    Function(dynamic error, dynamic trace)? onError,
+    required int timeout,
+    required bool continueEmittingOnBadCert,
+    io.SecurityContext? context,
+  });
+  Future<io.Socket?> upgradeConnection({
+    required int timeout,
+    required bool continueEmittingOnBadCert,
+    io.SecurityContext? context,
+  });
+  void write(dynamic data);
+  void destroy() {
+    if (ioSocket != null) {
+      ioSocket!.destroy();
+    }
+    subscription.cancel();
+  }
+}
