2a94ef2ece2fcd4ac7860677300769ee0ba80075
vsevolod <vsevex@gmail.com>
2023-12-14 03:01:59 +0400

    change example

--- example/main.dart
+++ example/main.dart
@@ -5,14 +5,37 @@ import 'package:connecta/connecta.dart';
 Future<void> main() async {
   final connecta = Connecta(
     ConnectaToolkit(
-      hostname: 'localhost',
-      certificatePath: 'public/cert.pem',
-      keyPath: 'public/key.pem',
+      hostname: 'example.org',
+      port: 5222,
+      connectionType: ConnectionType.upgradableTcp,
     ),
   );
 
-  await connecta.connect(onData: (value) => log(value.toString()));
-  await connecta.upgradeConnection();
+  await connecta.connect(
+    ConnectaListener(onData: (data) async => _handleData(connecta, data)),
+  );
+  connecta.send(
+    "<stream:stream to='example.org' xmlns:stream='http://etherx.jabber.org/streams' xmlns='jabber:client' xml:lang='en' version='1.0'>",
+  );
+}
 
-  connecta.send('hert');
+Future<void> _handleData(
+  Connecta connecta,
+  List<int> data,
+) async {
+  final rawData = String.fromCharCodes(data);
+  if (rawData.contains(
+    "<starttls xmlns='urn:ietf:params:xml:ns:xmpp-tls'><required/></starttls>",
+  )) {
+    connecta.send('<starttls xmlns="urn:ietf:params:xml:ns:xmpp-tls" />');
+  } else if (rawData == "<proceed xmlns='urn:ietf:params:xml:ns:xmpp-tls'/>") {
+    await connecta.upgradeConnection(
+      listener:
+          ConnectaListener(onData: (data) => log(String.fromCharCodes(data))),
+    );
+
+    connecta.send(
+      "<stream:stream to='example.org' xmlns:stream='http://etherx.jabber.org/streams' xmlns='jabber:client' xml:lang='en' version='1.0'>",
+    );
+  }
 }
